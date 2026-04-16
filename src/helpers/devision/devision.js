80d6cf5811aff319e6e6f608c0cafd6431fd0153
Andrii Kushch <akushch@alliancedigital.tech>
2026-04-16 10:44:46 +0300

    add division

---
+++ src/helpers/devision/devision.js
@@ -0,0 +1,1 @@
+export const devision = (a, b) => a / b;
---
+++ src/helpers/devision/devision.test.js
@@ -0,0 +1,7 @@
+import { devision } from './devision';
+
+describe('division', () => {
+    it('should divide 4 by 2 to equal 2', () => {
+        expect(devision(4, 2)).toBe(2);
+    });
+});
